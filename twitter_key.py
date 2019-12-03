37652212ceef70fc968340292036e01ec12cbf33
Kai Bogdanovich (incognitoPants) <k.boggyvich@gmail.com>
2019-12-02 18:56:51 -0800

    Removed twitter_key from repo

--- twitter_key.py
+++
@@ -1,7 +0,0 @@
-# Credentials
-twitter_keys = {
-    'consumer_key':     'qsjsijrJU2h4AdmZ7l5F1gQr4',
-    'consumer_secret':  'MZrmTSBpbDf122z1ClzlZBJjsOob1vc3dfwzX5ndll3Q3JQC93',
-    'access_token_key': '1196644242061348864-Rfaw5WtRBztjIuX52L3qvURbFrUv87',
-    'access_token_secret':  'yVhZP5zX5Dbcqzl0AwXT6yF70scIF5yEcFfFW7CMhsvz7'
-}
