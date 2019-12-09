1072830ed56d2f7f77dfa37d1c6701ecd1cd0185
jooyolm2 <55519037+jooyolm2@users.noreply.github.com>
2019-12-08 21:15:32 -0800

    Add files via upload
    twitter credentials to run tweepy streamer w/o Cursor.py

---
+++ twitter_credentials.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 14:05:17 2019
+
+@author: maengjy
+"""
+
+# Credentials to access Twitter API (this is Kai's Twitter API key)
+ACCESS_TOKEN = '1196644242061348864-Rfaw5WtRBztjIuX52L3qvURbFrUv87'
+ACCESS_TOKEN_SECRET = 'yVhZP5zX5Dbcqzl0AwXT6yF70scIF5yEcFfFW7CMhsvz7'
+CONSUMER_KEY = 'qsjsijrJU2h4AdmZ7l5F1gQr4'
+CONSUMER_SECRET = 'MZrmTSBpbDf122z1ClzlZBJjsOob1vc3dfwzX5ndll3Q3JQC93'
+
+
+# Credentials to access Twitter API (this is Kai's Twitter API key)
+#twitter_keys = {
+#    'consumer_key':     'qsjsijrJU2h4AdmZ7l5F1gQr4',
+#    'consumer_secret':  'MZrmTSBpbDf122z1ClzlZBJjsOob1vc3dfwzX5ndll3Q3JQC93',
+#    'access_token_key': '1196644242061348864-Rfaw5WtRBztjIuX52L3qvURbFrUv87',
+#    'access_token_secret':  'yVhZP5zX5Dbcqzl0AwXT6yF70scIF5yEcFfFW7CMhsvz7'
+#}
