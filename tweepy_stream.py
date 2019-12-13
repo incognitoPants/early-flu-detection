9c3e679e89c0a7bb55413f06b9c9f149be5dfa85
Kai Bogdanovich (incognitoPants) <k.boggyvich@gmail.com>
2019-12-12 20:24:00 -0800

    JSON to CSV conversion fix

--- tweepy_stream.py
+++ tweepy_stream.py
@@ -6,19 +6,19 @@ from twitter_key import twitter_keys
 auth = tw.OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
 auth.set_access_token(twitter_keys['access_token_key'], twitter_keys['access_token_secret'])
 
+token = rtweet::create_token(app_name,cons_key,cons_sec,acc_tok,acc_sec)
 api = tw.API(auth)
-#for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-    #print(tweet.text)
 
 # Search words filtering retweets
 search_words = '#flu' + '-filter:retweets'
-date_since = "2019-01-30"
+date_since = "2015-12-31"
+date_until = "2016-12-31"
 
 # Collecting tweets
-tweets = tw.Cursor(api.search, q=search_words, lang='en', since=date_since).items()
+tweets = tw.Cursor(api.search, q=search_words, lang='en', since=date_since, until=date_until).items()
 
 a = 0
 
-# Print Tweets
+# Count Tweets
 for tweet in tweets:
     a = a+1
