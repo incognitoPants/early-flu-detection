6e9566f384ff1c20fad0107b6d0e8fada8c8de45
Kai Bogdanovich (incognitoPants) <k.boggyvich@gmail.com>
2019-12-19 08:37:12 -0800

    Added more comments and descriptions

--- add_sentiment.py
+++ add_sentiment.py
@@ -1,9 +1,23 @@
+"""
+**** Documentation ****
+Use this to convert an existing CSV file containing tweets. Please note that this assumes the column names from
+the twitter data were not modified.
+
+How to use:
+    Assign the address of your CSV to variable input_file. We recommend keeping files in the Input directory.
+
+Result:
+    An output file named as output_file.csv will be created in the /Output directory.
+
+"""
 from textblob import TextBlob
 import pandas as pd
 import re
 
-file = "tweets_ns.csv" # replace with the CSV you want to add sentiment
-df = pd.read_csv(file)
+input_file = "tweets_ns.csv" # replace with the CSV you want to add sentiment
+output_name = "tweets_added_sentiment.csv"
+output_file = "Output/" + output_name
+df = pd.read_csv(input_file)
 snt_list = []
 for tweet in df["text"]:
     tweet = ' '.join(
@@ -23,4 +37,4 @@ for tweet in df["text"]:
 df["sentiment"] = snt_list
 
 # headers will not be included since the CSV's already have them.
-df.to_csv("tweets_added_sentiment.csv", mode='w', index=False)
+df.to_csv(output_file, mode='w', index=False)
