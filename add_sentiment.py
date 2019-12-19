47c365e03f0d6a9e4e43bc33b5650c9949d0c9db
Kai Bogdanovich (incognitoPants) <k.boggyvich@gmail.com>
2019-12-18 18:50:15 -0800

    Code to run for CSV with no sentiment column

---
+++ add_sentiment.py
@@ -0,0 +1,25 @@
+from textblob import TextBlob
+import pandas as pd
+import re
+
+
+file = "tweets_ns.csv" # replace with the CSV you want to add sentiment
+df = pd.read_csv(file)
+snt_list = []
+for tweet in df["text"]:
+    tweet = ' '.join(
+        re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    s = TextBlob(tweet)
+    if s.sentiment.polarity > 0:
+        snt = 'positive'
+        print(snt)
+    elif s.sentiment.polarity == 0:
+        snt = 'neutral'
+        print(snt)
+    else:
+        snt = 'negative'
+        print(snt)
+
+    snt_list.append(snt)
+df["sentiment"] = snt_list
+df.to_csv("tweets_added_sentiment.csv", mode='w', index=False)
